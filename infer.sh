634c43e2a6dbb5b723affd368e5bf5a47b9492d4
Haodong LI <lebronlihd@gmail.com>
2024-10-06 01:31:24 -0400

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -16,9 +16,9 @@ export TEST_IMAGES="assets/in-the-wild_example"
 
 CUDA_VISIBLE_DEVICES=$CUDA python infer.py \
         --pretrained_model_name_or_path=$CHECKPOINT_DIR \
-        --half_precision \
         --prediction_type="sample" \
         --seed=42 \
+        --half_precision \
         --input_dir=$TEST_IMAGES \
         --task_name=$TASK_NAME \
         --mode=$MODE \
